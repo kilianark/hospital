f168dd9d48f6aad8304042ee1b67f8b85df49bcb
Ji-Liu Li-Shi <li.ji.ext@inetum.com>
2024-07-16 09:06:51 +0200

    modificadors obligatoris

--- ApiHospital/Models/Patient.cs
+++ ApiHospital/Models/Patient.cs
@@ -3,10 +3,10 @@ namespace ApiHospital.Models
     public class Patient
     {
         public int Id { get; set; }
-        public string Nif { get; set; }
-        public string Name { get; set; }
+        public required string Nif { get; set; }
+        public required string Name { get; set; }
 
-        public string Surname { get; set; }
+        public required string Surname { get; set; }
 
         public int? BedId { get; set; }
 
