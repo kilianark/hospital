87a07a5e79d0754cdaca6c0e921371ec1f97e9a2
Lopez-Nuno-Riera Marta <marta.lopez-nuno.ext@inetum.com>
2024-07-18 12:37:21 +0200

    login duplicat

--- angular-front/src/app/app.config.ts
+++ angular-front/src/app/app.config.ts
@@ -1,8 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, NgModule, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
+import { AppRoutingModule, routes } from './app.routes';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BrowserModule } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
 };
+
+
+export class AppModule { }
