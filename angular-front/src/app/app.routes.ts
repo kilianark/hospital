045f0545f205cc0d9982b64a0c11c70131ffbb8d
Kilian <kilian08momo@gmail.com>
2024-07-22 10:44:25 +0200

    added create and search components for rooms, and added path in routes

--- angular-front/src/app/app.routes.ts
+++ angular-front/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { LoginComponent } from './login/login.component';
 import { SearchPatientComponent } from './patient/search/search.component';
 import { CreatePatientComponent } from './patient/create/create.component';
+import { SearchRoomComponent } from './room/search/search.component';
+import { CreateRoomComponent } from './room/create/create.component';
 
 // lista de rutes per exemple per posar en Routes
 // { path: 'nomdel enllaç', component: nomVariable(per defecte es posa, en el navbar.component.ts) } por defecto va a la pagina de inicio
@@ -12,10 +14,12 @@ import { CreatePatientComponent } from './patient/create/create.component';
 //navbar li importem els dos links i lactive.
 export const routes: Routes = [
 
-    { path: 'search-patient', component: SearchPatientComponent},
-    { path: 'create-patient', component: CreatePatientComponent},
-    { path: 'home', component: HomeComponent},
-    { path: 'login', component: LoginComponent},
+    { path: 'search-patient', component: SearchPatientComponent },
+    { path: 'create-patient', component: CreatePatientComponent },
+    { path: 'search-room', component: SearchRoomComponent },
+    { path: 'create-room', component: CreateRoomComponent},
+    { path: 'home', component: HomeComponent },
+    { path: 'login', component: LoginComponent },
     { path: '', redirectTo: '/login', pathMatch: 'full' }
 
 ];
---
+++ angular-front/src/app/room/search/search.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../../header/header.component";
+
+@Component({
+  selector: 'app-search',
+  standalone: true,
+  imports: [HeaderComponent],
+  templateUrl: './search.component.html',
+  styleUrl: './search.component.css'
+})
+export class SearchRoomComponent {
+
+}
