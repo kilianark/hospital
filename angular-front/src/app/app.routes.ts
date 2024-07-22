a14d8b6d78972ae8aed277a83c3a6d72ebbaac40
Maria Manzano <mmanzanoroldan@gmail.com>
2024-07-22 12:16:30 +0200

    Component alta pacient i les seves rutes

--- angular-front/src/app/app.routes.ts
+++ angular-front/src/app/app.routes.ts
@@ -6,11 +6,12 @@ import { SearchPatientComponent } from './patient/search/search.component';
 import { CreatePatientComponent } from './patient/create/create.component';
 import { SearchRoomComponent } from './room/search/search.component';
 import { CreateRoomComponent } from './room/create/create.component';
+import { DischargeComponent } from './patient/discharge/discharge.component';
 
 // lista de rutes per exemple per posar en Routes
 // { path: 'nomdel enllaç', component: nomVariable(per defecte es posa, en el navbar.component.ts) } por defecto va a la pagina de inicio
 
-// en navbar.components.ts barra de navegació, li posem un atribut de tipus routerLink="/...", routerLinkActive="active" <- atribut, que en css li estilitzem ....> .nav-link.active, i tambe ho importem al templateURL, i style que li assignem 
+// en navbar.components.ts barra de navegació, li posem un atribut de tipus routerLink="/...", routerLinkActive="active" <- atribut, que en css li estilitzem ....> .nav-link.active, i tambe ho importem al templateURL, i style que li assignem
 //navbar li importem els dos links i lactive.
 export const routes: Routes = [
 
@@ -18,6 +19,7 @@ export const routes: Routes = [
     { path: 'create-patient', component: CreatePatientComponent },
     { path: 'search-room', component: SearchRoomComponent },
     { path: 'create-room', component: CreateRoomComponent},
+    { path: 'discharge-patient', component: DischargeComponent},
     { path: 'home', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: '', redirectTo: '/login', pathMatch: 'full' }
