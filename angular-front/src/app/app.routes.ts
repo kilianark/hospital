284df812b833477623e405ecd475f0050bb1a9e1
Lopez-Nuno-Riera Marta <marta.lopez-nuno.ext@inetum.com>
2024-07-24 13:29:00 +0200

    fixing merge

--- angular-front/src/app/app.routes.ts
+++ angular-front/src/app/app.routes.ts
@@ -7,7 +7,6 @@ import { CreatePatientComponent } from './patient/create/create.component';
 import { SearchBedComponent } from './room/search-bed/search-bed.component';
 import { SearchRoomComponent } from './room/search-room/search-room.component';
 import { RecordComponent } from './patient/record/record.component';
-import { SidebarComponent } from './navigate/sidebar/sidebar.component';
 
 // lista de rutes per exemple per posar en Routes
 // { path: 'nomdel enllaç', component: nomVariable(per defecte es posa, en el navbar.component.ts) } por defecto va a la pagina de inicio
@@ -15,14 +14,7 @@ import { SidebarComponent } from './navigate/sidebar/sidebar.component';
 // en navbar.components.ts barra de navegació, li posem un atribut de tipus routerLink="/...", routerLinkActive="active" <- atribut, que en css li estilitzem ....> .nav-link.active, i tambe ho importem al templateURL, i style que li assignem
 //navbar li importem els dos links i lactive.
 export const routes: Routes = [
-<<<<<<< HEAD
-<<<<<<< HEAD
-   
-=======
->>>>>>> bbcc520024ca263a8a81c355462c8ba008f3f838
-=======
-    { path: 'sidebar', component: SidebarComponent }, //temporal para visualizar
->>>>>>> d2477264181b46f36e703f04e1b1057149dfdfa3
+
     { path: 'patient', component: RecordComponent },
     { path: 'search-patient', component: SearchPatientComponent },
     { path: 'create-patient', component: CreatePatientComponent },
