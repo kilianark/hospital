36412f4d947ba339543c9a00d2a0c7514d882d14
Lopez-Nuno-Riera Marta <marta.lopez-nuno.ext@inetum.com>
2024-07-29 12:07:20 +0200

    user menu changes

--- angular-front/src/app/navigate/header/header.component.ts
+++ angular-front/src/app/navigate/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 @Component({
@@ -9,12 +9,27 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
     imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive]
   })
 
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy{
   title = 'MedicaPlus';
   nav1 = 'Urgencias';
   nav2 = 'Consultas';
 
   isMenuOpen = false;
+
+  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
+
+  ngOnInit() {
+    // Añadir un listener para hacer clic en el documento
+    this.renderer.listen('document', 'click', (event: Event) => {
+      if (this.isMenuOpen && !this.elRef.nativeElement.contains(event.target)) {
+        this.isMenuOpen = false;
+      }
+    });
+  }
+
+  ngOnDestroy() {
+  
+  }
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
