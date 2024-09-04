263f4ba011c98fd9b71db1c3e97ecec6e10c23ca
Maria Manzano <mmanzanoroldan@gmail.com>
2024-09-04 10:31:11 +0200

    Feta la base de la busqueda de habitacions

--- angular-front/src/app/room/search-room/search-room.component.ts
+++ angular-front/src/app/room/search-room/search-room.component.ts
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrl: './search-room.component.css'
 })
 export class SearchRoomComponent {
+  title = 'Busqueda habitaciones:'
 
+  onSubmit() {
+    //canviar esto por el paciente/s encontrado por la api
+    console.log('Formulario enviado');
+  }
 }
