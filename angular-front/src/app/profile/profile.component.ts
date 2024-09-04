70b0a078b2608f89e227207766ca5c3c4186994c
MelieCas <vcasares5@gmail.com>
2024-09-04 12:08:07 +0200

    Profile component html

--- angular-front/src/app/profile/profile.component.ts
+++ angular-front/src/app/profile/profile.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+
+import { countries } from '../shared/store/country-data.store';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, RouterOutlet],
+  imports: [RouterLink, RouterLinkActive, RouterOutlet, CommonModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
 export class ProfileComponent {
+  countries: any = countries;
 
 }
