00504c2d91fe612f03f45d4a06a7002330b77725
Maria Manzano <mmanzanoroldan@gmail.com>
2024-09-05 10:11:05 +0200

    Cambiat el llistat manual dels llits

---
+++ angular-front/src/app/room/search-room/roomsearch.ts
@@ -0,0 +1,8 @@
+export interface Roomsearch{
+  numero: string;
+  tipo: string;
+  planta: number;
+  area: string;
+  capacidad: number;
+  disponible: string;
+}
--- angular-front/src/app/room/search-room/search-room.component.ts
+++ angular-front/src/app/room/search-room/search-room.component.ts
@@ -1,14 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Roomsearch } from './roomsearch';
 
 @Component({
   selector: 'app-search-room',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './search-room.component.html',
   styleUrl: './search-room.component.css'
 })
 export class SearchRoomComponent {
   title = 'Búsqueda Habitaciones:'
+
+  room: Roomsearch[] = [
+    { numero: "001", tipo: "Box", planta: 0, area: "Urgencias", capacidad: 1, disponible: "Si" },
+    { numero: "100", tipo: "Cubículo", planta: 1, area: "UCI", capacidad: 1, disponible: "Si" }
+  ]
 
   onSubmit() {
     //canviar esto por el paciente/s encontrado por la api
