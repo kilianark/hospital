bd2ad2b9cb411d0a50d4022442dd6b9254380ccc
Kilian <kilian08momo@gmail.com>
2024-09-06 09:35:35 +0200

    starting to add the form to record patient

--- angular-front/src/app/components/recordpatient/record.component.ts
+++ angular-front/src/app/components/recordpatient/record.component.ts
@@ -1,9 +1,27 @@
+import { ReactiveFormsModule, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-record',
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './record.component.html',
   styleUrls: ['./record.component.css']
 })
 export class RecordComponent {
+  patientForm: FormGroup;
+  constructor(private formBuilder: FormBuilder) {
+    this.patientForm = this.formBuilder.group({
+      dni: ['', [Validators.required, Validators.pattern(/^\d{8}[A-Z]$/)]],
+      cip: ['', [Validators.pattern(/^\d{8}[A-Z]{2}$/)]],
+      name: ['', [Validators.required]],
+      birth: ['', [Validators.required]],
+      surname1: ['', [Validators.required]],
+      phone: ['', [Validators.required, Validators.pattern(/^\d{9}$/)]],
+      email: ['', [Validators.email]],
+      country: ['Spain', [Validators.required]],
+      emergencyContact: ['', [Validators.pattern(/^\d{9}$/)]],
+      sex: ['', [Validators.required]]
+    });
+  }
 }
