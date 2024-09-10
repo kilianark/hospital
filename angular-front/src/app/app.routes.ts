a546e7fcec30b70144c51141a1a391492870f5d2
Kilian <kilian08momo@gmail.com>
2024-09-10 12:24:58 +0200

    app.routes cleaned

--- angular-front/src/app/app.routes.ts
+++ angular-front/src/app/app.routes.ts
@@ -1,35 +1,19 @@
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/components/home.component';
 import { NgModule } from '@angular/core';
+
 import { MatDialogModule } from '@angular/material/dialog';
+
 import { LoginComponent } from './pages/login/components/login.component';
-import { SearchPatientComponent } from './pages/home/components/patient/search/search.component';
-import { CreatePatientComponent } from './pages/home/components/patient/create/create.component';
-import { SearchRoomComponent } from './pages/home/components/room/search/search.component';
-import { ConfirmComponent } from './components/confirm/confirm.component';
-import { ProfileComponent } from './pages/home/components/profile/profile.component';
-import { ManagePatientComponent } from './pages/home/components/patient/manage/manage.component';
 import { HomeModule } from './modules/home.module/home.module';
 
-// lista de rutes per exemple per posar en Routes
-// { path: 'nomdel enllaç', component: nomVariable(per defecte es posa, en el navbar.component.ts) } por defecto va a la pagina de inicio
-
-// en navbar.components.ts barra de navegació, li posem un atribut de tipus routerLink="/...", routerLinkActive="active" <- atribut, que en css li estilitzem ....> .nav-link.active, i tambe ho importem al templateURL, i style que li assignem
-//navbar li importem els dos links i lactive.
 export const routes: Routes = [
     { 
       path: 'home',
-      //component: HomeComponent,
       loadChildren: () => import('./modules/home.module/home.module').then(m => m.HomeModule) // Lazy load del módulo Home
     },
     { path: 'login', component: LoginComponent },
-    //{ path: 'manage-patient', component: ManagePatientComponent},
-    //{ path: 'create-patient', component: CreatePatientComponent },
-    
-    //{ path: 'profile', component: ProfileComponent },
     { path: '', redirectTo: '/login', pathMatch: 'full' },
-    { path: '**', redirectTo: '/login' } // quan la ruta es incorrecte o qualsevol cosa
-
+    { path: '**', redirectTo: '/login' }
 ];
 
 @NgModule({
