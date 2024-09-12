42ac6fcbdb2956db2e58ca1685d841ef6c883003
Kilian <kilian08momo@gmail.com>
2024-09-12 12:23:37 +0200

    rectified patient interface atributes

--- angular-front/src/app/interfaces/patient.interface.ts
+++ angular-front/src/app/interfaces/patient.interface.ts
@@ -1,12 +1,12 @@
 export interface PatientInterface{
-    codigo: number;
-    nombre: string;
-    apellido1: string;
-    apellido2: string;
-    telefono: number;
-    edad: number;
+    code: number;
+    name: string;
+    surname1: string;
+    surname2: string;
+    phone: number;
+    age: number;
     birthdate: Date;
-    estado: string;
-    tipo: string;
-    idcama: string; //de momento para acepta "-".
+    status: string;
+    type: string;
+    idBed: string; //de momento para acepta "-".
 }
