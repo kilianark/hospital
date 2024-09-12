fcbfeddc216b4c8a5a6419b476ac1604cf1c1a0a
Lopez-Nuno-Riera Marta <marta.lopez-nuno.ext@inetum.com>
2024-09-12 13:23:40 +0200

    interfaz doctor

---
+++ angular-front/src/app/interfaces/doctor.interface.ts
@@ -0,0 +1,18 @@
+export interface DoctorInterface{
+    
+    id: number;
+    doctor_code: number;
+    name: string;
+    surname1: string;
+    surname2: string;
+    phone: number;
+    age: number;
+    birthdate: Date;
+    address: string;
+    dni: string;
+    cip: string;
+    email: string;
+    country: string;
+    sex: string;
+    speciality: string;
+}
