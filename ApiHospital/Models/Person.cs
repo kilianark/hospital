c67cb62062baf677ce32f1e313e78e33f2ea9adc
Lopez-Nuno-Riera Marta <marta.lopez-nuno.ext@inetum.com>
2024-09-16 11:01:15 +0200

    creación modelo persona

---
+++ ApiHospital/Models/Person.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiHospital.Models;
+public interface Person {
+
+    [Key]
+    public int Id { get; set;}
+    public string Dni { get; set; }
+    public string Name { get; set; }
+    public string Surname1 { get; set; }
+    public string? Surname2 { get; set; }
+    public DateOnly BirthDate { get; set; }
+    public string Country { get; set; }
+    public string? Adress {get; set;}
+    public string Phone {get; set; }
+    public string Email {get; set;}
+    public string CIP {get; set;}
+    public string Sex {get; set;}
+}
