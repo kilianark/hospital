719184f526cd9376e650e7e1d8af70a006763e54
Kilian <kilian08momo@gmail.com>
2024-09-16 10:59:13 +0200

    si

--- ApiHospital/Interfaces/IPersona.cs
+++ ApiHospital/Interfaces/IPersona.cs
@@ -2,6 +2,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace ApiHospital.Interfaces;
 public interface IPersona {
+    public int Id { get; set; }
     public string Dni { get; set; }
     public string Name { get; set; }
     public string Surname1 { get; set; }
