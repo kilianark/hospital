8976045efcf8de40deb2e5a745b3a997651b7e0d
Kilian <kilian08momo@gmail.com>
2024-09-17 10:14:43 +0200

    modif DTO

--- hospitalDTO/DTOapi/PatientDTO.cs
+++ hospitalDTO/DTOapi/PatientDTO.cs
@@ -1,19 +1,21 @@
 namespace hospitalDTO.DTOapi {
     public class PatientDTO {
-    public required int PatientCode {get; set;}
-    public required string Dni { get; set; }
-    public required string Name { get; set; }
-    public required string Surname1 { get; set; }
-    public string? Surname2 { get; set; }
-    public required DateOnly BirthDate { get; set; }
-    public required string Country { get; set; } // caldra afegir també en els DTO, API, Controladors, etc
-    public string? Adress {get; set;}
-    public required string Phone {get; set; }
-    public required string Email {get; set;}
-    public required string CIP {get; set;}
-    public required string Gender {get; set;}
-    public required string Status { get; set; }
-    public required string StatusType { get; set; }
-    public int? BedId { get; set; }
+        public required int PatientCode {get; set;}
+        public required string Dni { get; set; }
+        public required string Name { get; set; }
+        public required string Surname1 { get; set; }
+        public string? Surname2 { get; set; }
+        public required int Age { get; set; }
+        public required DateOnly BirthDate { get; set; }
+        public required string Country { get; set; } // caldra afegir també en els DTO, API, Controladors, etc
+        public string? Address {get; set;}
+        public required string Phone {get; set; }
+        public required string Email {get; set;}
+        public required string CIP {get; set;}
+        public required string Gender {get; set;}
+        public string? EmergencyContact { get; set; }
+        public required string Status { get; set; }
+        public string? Reason { get; set; }
+        public int? BedId { get; set; }
     }
 }
