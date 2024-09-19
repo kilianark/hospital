c776dfed8358d097c3ef4895ce94a592d6da0457
Kilian <kilian08momo@gmail.com>
2024-09-19 09:31:26 +0200

    si

--- angular-front/src/app/pages/home/components/patient/search/search.component.ts
+++ angular-front/src/app/pages/home/components/patient/search/search.component.ts
@@ -3,7 +3,6 @@ import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
 import { RecordComponent } from '../../../../../components/recordpatient/record.component';
 import { PatientInterface } from '../../../../../interfaces/patient.interface';
-import { countries } from '../../../../../store/country-data.store';
 import { PatientService } from '../../../../../services/patient.service';
 
 @Component({
@@ -15,16 +14,13 @@ export class SearchPatientComponent implements OnInit {
   title = 'Búsqueda Pacientes:';
 
   patient: PatientInterface[] = [
-    //{ id: 0, patient_code: 1234567, name: "Juan", surname1: "Martínez", surname2: "López", dni: "", cip: "", gender: "", phone: "631238791", email: "", age: 34, birthdate: new Date("1990-09-12"), country: countries[0].name, status: "Ambulatorio", address: "", emergencyContact:""},
-    //{ id: 1, patient_code: 7654321, name: "Maria", surname1: "Pérez", surname2: "Castro", dni: "", cip: "", gender: "", phone: "621655788", email: "", age: 54, birthdate: new Date("1970-09-12"), country: countries[0].name, status: "Hospitalizado", address: "", emergencyContact:"", bed_id: 4011}
   ];
-
+ 
   constructor(public dialog: MatDialog, private router: Router, private patientService: PatientService) {}
 
   ngOnInit(): void {
     this.patientService.getPatientData().subscribe(data => {
       this.patient = data;
-      console.log(this.patient);
     })
   }
 
@@ -44,5 +40,12 @@ export class SearchPatientComponent implements OnInit {
 
   goToManage(patientId: number) {
     this.router.navigate(['/home/patient/manage', { id: patientId }]);
+  }
+
+  onSubmit() {
+    this.patientService.getPatientData("si", 1).subscribe(data => {
+      this.patient = data;
+      console.log(data);
+    })
   }
 }
