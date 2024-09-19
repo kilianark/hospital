ab7fd0fd34356ada0d2b92f1dd96b963bfd88044
MelieCas <vcasares5@gmail.com>
2024-09-19 09:23:44 +0200

    Patient Service modified

--- angular-front/src/app/pages/home/components/patient/search/search.component.ts
+++ angular-front/src/app/pages/home/components/patient/search/search.component.ts
@@ -15,8 +15,6 @@ export class SearchPatientComponent implements OnInit {
   title = 'Búsqueda Pacientes:';
 
   patient: PatientInterface[] = [
-    //{ id: 0, patient_code: 1234567, name: "Juan", surname1: "Martínez", surname2: "López", dni: "", cip: "", gender: "", phone: "631238791", email: "", age: 34, birthdate: new Date("1990-09-12"), country: countries[0].name, status: "Ambulatorio", address: "", emergencyContact:""},
-    //{ id: 1, patient_code: 7654321, name: "Maria", surname1: "Pérez", surname2: "Castro", dni: "", cip: "", gender: "", phone: "621655788", email: "", age: 54, birthdate: new Date("1970-09-12"), country: countries[0].name, status: "Hospitalizado", address: "", emergencyContact:"", bed_id: 4011}
   ];
 
   constructor(public dialog: MatDialog, private router: Router, private patientService: PatientService) {}
@@ -30,16 +28,16 @@ export class SearchPatientComponent implements OnInit {
 
 
 
-  openDialog(surname1: string) {
+  openDialog(patientCode: number) {
     let popupRef = this.dialog.open(RecordComponent, {
       width: '80%',
       height: '100%',
       maxWidth: '100vw',
-      panelClass: 'full-width-dialog'
+      panelClass: 'full-width-dialog',
+      data: patientCode
       
       
     });
-    popupRef.componentInstance.patientSurname = surname1;
   }
 
   goToManage(patientId: number) {
