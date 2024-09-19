4a1c9e53f2c9217a47aed347ad23ba989783c176
KumiGaoo <Kumigaoo@gmail.com>
2024-09-19 12:53:32 +0200

    Al menos funciona el translate, no me pidais mas

---
+++ angular-front/src/app/pipe/hospitalized-area.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { HospitalizedAreaPipe } from './hospitalized-area.pipe';
+
+describe('HospitalizedAreaPipe', () => {
+  it('create an instance', () => {
+    const pipe = new HospitalizedAreaPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
