c19e80b1bae493334b60a8a4020205cf3e3fb42e
martalnr <marta.lopeznuno@gmail.com>
2024-09-25 09:42:23 +0200

    cambios room type. creación servicio room

---
+++ angular-front/src/app/services/room.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { RoomInterface } from '../pages/home/interfaces/room.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoomService {
+
+  constructor() { }
+}
