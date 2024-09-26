df4dd1c7211167d1303cd573961616221ec6bd15
KumiGaoo <Kumigaoo@gmail.com>
2024-09-26 12:21:35 +0200

    Hospital y urgencias junto el component

--- angular-front/src/app/pipe/operating-room-area.pipe.ts
+++ angular-front/src/app/pipe/operating-room-area.pipe.ts
@@ -2,17 +2,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { OperatingRoomArea } from '../enums/operatingRoom-area.enum';
 
 @Pipe({
-  name: 'operatingRoomArea'
+  name: 'operatingRoomArea',
 })
 export class OperatingRoomAreaPipe implements PipeTransform {
-
   private readonly enumLabels: { [key: string]: string } = {
-    [OperatingRoomArea.SalaOperacion]: 'Sala de operación',
-    [OperatingRoomArea.SalaReanimacion]: 'Sala de eanimación',
-    [OperatingRoomArea.SalaEspera]: 'Sala de espera'
-  }
+    [OperatingRoomArea.SalaOperacion]: 'Sala de operaciones',
+    [OperatingRoomArea.SalaReanimacion]: 'Sala de reanimación',
+    [OperatingRoomArea.SalaEspera]: 'Sala de espera',
+  };
   transform(value: OperatingRoomArea): string {
     return this.enumLabels[value];
   }
-
 }
---
+++ angular-front/src/app/pipe/urgency-area.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { UrgencyAreaPipe } from './urgency-area.pipe';
+
+describe('UrgencyAreaPipe', () => {
+  it('create an instance', () => {
+    const pipe = new UrgencyAreaPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
