9be7b28b1ebb532a04a232691026b7c7d5c0cb43
KumiGaoo <Kumigaoo@gmail.com>
2024-09-27 10:36:54 +0200

    No funcionaba una cosa del search room

--- angular-front/src/app/pages/home/components/room/search/search.component.ts
+++ angular-front/src/app/pages/home/components/room/search/search.component.ts
@@ -7,7 +7,7 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-search-room',
   templateUrl: './search.component.html',
-  styleUrl: './search.component.css'
+  styleUrl: './search.component.css',
 })
 export class SearchRoomComponent {
   title = 'Búsqueda Habitaciones:';
@@ -15,40 +15,53 @@ export class SearchRoomComponent {
   roomForm: FormGroup;
   isVisible: boolean = false;
 
-  constructor(private router: Router, private formBuilder: FormBuilder, private roomService: RoomService) {
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private roomService: RoomService
+  ) {
     this.roomForm = this.formBuilder.group({
       roomNumber: [''],
       floor: [''],
       area: [''],
       capacity: [''],
-      availability: ['']
+      availability: [''],
     });
   }
 
   onSubmit() {
     const searchFilters = this.roomForm.value;
 
-    const roomNumber = searchFilters.room_number ? parseInt(searchFilters.roomNumber, 10) : null;
-    const floor = searchFilters.floor ? parseInt(searchFilters.floor, 10) : null;
-    const capacity = searchFilters.capacity ? parseInt(searchFilters.capacity, 10) : null;
-    const availability = searchFilters.availability ? searchFilters.availability === 'true' : null;
+    const roomNumber = searchFilters.room_number
+      ? parseInt(searchFilters.roomNumber, 10)
+      : null;
+    const floor = searchFilters.floor
+      ? parseInt(searchFilters.floor, 10)
+      : null;
+    const capacity = searchFilters.capacity
+      ? parseInt(searchFilters.capacity, 10)
+      : null;
+    const availability = searchFilters.availability
+      ? searchFilters.availability === 'true'
+      : null;
 
-    this.roomService.searchRooms(
-      roomNumber,
-      floor,
-      searchFilters.area,
-      capacity,
-      availability
-    ).subscribe(
-      (rooms: RoomInterface[]) => {
-        this.room = rooms;
-        this.isVisible = true;
-      },
-      (error) => {
-        console.error('Error al buscar habitaciones:', error);
-      }
-    );
-
+    this.roomService
+      .searchRooms(
+        roomNumber,
+        floor,
+        searchFilters.area,
+        capacity,
+        availability
+      )
+      .subscribe(
+        (rooms: RoomInterface[]) => {
+          this.room = rooms;
+          this.isVisible = true;
+        },
+        (error) => {
+          console.error('Error al buscar habitaciones:', error);
+        }
+      );
   }
 
   goToManage(patientId: number | undefined) {
@@ -58,5 +71,8 @@ export class SearchRoomComponent {
       console.log(this.room[0]);
       console.log('No hay paciente asignado a esta habitación.');
     }
+  }
+  toggleDisplay() {
+    this.isVisible = true;
   }
 }
