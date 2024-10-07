7c2e53943518d476cbc41f290376d4b2f0fdbd7c
martalnr <marta.lopeznuno@gmail.com>
2024-10-07 13:15:31 +0200

    página crear/buscar room botones + sidebar crear hab

--- angular-front/src/app/pages/home/components/home.component.ts
+++ angular-front/src/app/pages/home/components/home.component.ts
@@ -2,10 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-home',
-    //standalone: true,
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
-    //imports: [HeaderComponent]
   })
 
 export class HomeComponent {
