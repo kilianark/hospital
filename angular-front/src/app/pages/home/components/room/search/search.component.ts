87a19be95864d2be1cab0180c8805f8e8af9c852
Kilian <kilian08momo@gmail.com>
2024-10-10 10:32:09 +0200

    zone added to search rooms

--- angular-front/src/app/pages/home/components/room/search/search.component.ts
+++ angular-front/src/app/pages/home/components/room/search/search.component.ts
@@ -5,77 +5,67 @@ import { Router } from '@angular/router';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
-  selector: 'app-search-room',
-  templateUrl: './search.component.html',
-  styleUrl: './search.component.css',
+	selector: 'app-search-room',
+	templateUrl: './search.component.html',
+	styleUrl: './search.component.css',
 })
 export class SearchRoomComponent {
-  title = 'Búsqueda Habitaciones:';
-  rooms: RoomInterface[] = [];
-  roomForm: FormGroup;
-  isVisible: boolean = false;
+	title = 'Búsqueda Habitaciones:';
+	rooms: RoomInterface[] = [];
+	roomForm: FormGroup;
+	isVisible: boolean = false;
 
-  constructor(
-    private router: Router,
-    private formBuilder: FormBuilder,
-    private roomService: RoomService
-  ) {
-    this.roomForm = this.formBuilder.group({
-      roomNumber: [''],
-      floor: [''],
-      area: [''],
-      capacity: [''],
-      availability: [''],
-    });
-  }
+  	constructor( private router: Router, private formBuilder: FormBuilder, private roomService: RoomService) {
+		this.roomForm = this.formBuilder.group({
+      		roomNumber: [''],
+			floor: [''],
+			zone: [''],
+			area: [''],
+			capacity: [''],
+			availability: [''],
+    	});
+  	}
 
-  onSubmit() {
-    const searchFilters = this.roomForm.value;
+	onSubmit() {
+		const searchFilters = this.roomForm.value;
 
-    const roomNumber = searchFilters.roomNumber
-      ? parseInt(searchFilters.roomNumber, 10)
-      : null;
-    const floor = searchFilters.floor
-      ? parseInt(searchFilters.floor, 10)
-      : null;
-    const capacity = searchFilters.capacity
-      ? parseInt(searchFilters.capacity, 10)
-      : null;
-    const availability = searchFilters.availability
-      ? searchFilters.availability === 'true'
-      : null;
+		const roomNumber = searchFilters.roomNumber ? parseInt(searchFilters.roomNumber, 10) : null;
 
-    this.roomService
-      .searchRooms(
-        roomNumber,
-        floor,
-        searchFilters.area,
-        capacity,
-        availability
-      )
-      .subscribe(
-        (rooms: RoomInterface[]) => {
-          this.rooms = rooms;
-          this.isVisible = true;
-        },
-        (error) => {
-          console.error('Error al buscar habitaciones:', error);
-        }
-      );
-  }
+		const floor = searchFilters.floor ? parseInt(searchFilters.floor, 10) : null;
 
+		const capacity = searchFilters.capacity ? parseInt(searchFilters.capacity, 10) : null;
+		
+		const availability = searchFilters.availability ? searchFilters.availability === 'true' : null;
 
-  
-    goToRooms(roomId: number) {
-      if (roomId !== undefined) {
-        this.router.navigate(['/home/room/beds', roomId]);
-      } else {
-        console.log(this.rooms[0]);
-        console.log('No hay camas asignadas a esta habitación.');
-      }
-    }
+		this.roomService
+		.searchRooms(
+			roomNumber,
+			floor,
+			searchFilters.area,
+			capacity,
+			availability
+		)
+		.subscribe(
+			(rooms: RoomInterface[]) => {
+			this.rooms = rooms;
+			this.isVisible = true;
+			},
+			(error) => {
+			console.error('Error al buscar habitaciones:', error);
+			}
+		);
+	}
 
-  toggleDisplay() {
-    this.isVisible = true;
-  }
+	goToRooms(roomId: number) {
+		if (roomId !== undefined) {
+			this.router.navigate(['/home/room/beds', roomId]);
+		} else {
+			console.log(this.rooms[0]);
+			console.log('No hay camas asignadas a esta habitación.');
+		}
+	}
+
+	toggleDisplay() {
+		this.isVisible = true;
+	}
 }
