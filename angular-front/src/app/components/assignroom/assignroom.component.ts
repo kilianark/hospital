09853cf05104dfae145c2e6a1a6370c93cabcabc
Kilian <kilian08momo@gmail.com>
2024-11-05 12:58:21 +0100

    ya hace el get correctamente

--- angular-front/src/app/components/assignroom/assignroom.component.ts
+++ angular-front/src/app/components/assignroom/assignroom.component.ts
@@ -12,7 +12,7 @@ import { ChangeDetectionStrategy } from '@angular/core';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
-import { MatDialog } from '@angular/material/dialog';
+import { MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { RecordComponent } from '../../components/recordpatient/record.component';
 @Component({
   selector: 'app-record',
@@ -25,7 +25,7 @@ import { RecordComponent } from '../../components/recordpatient/record.component
 export class AssignRoom implements OnInit {
   title = 'Gestión de camas: Habitación ';
   // id rebut -> obté valor per route
-  roomId: number | null = null;
+  roomId: number;
   // data per convenció reb l'instància (RoomInf)
   room!: RoomInterface;
   // obtenim tot el llistar d'interfaces de llit
@@ -40,6 +40,7 @@ export class AssignRoom implements OnInit {
   // quan rebem una data interface de paciente d'allà obtenim, id, nomSur, i el codiPatient.
 
   constructor(
+    @Inject(MAT_DIALOG_DATA) private data: number,
     public dialog: MatDialog,
     private route: ActivatedRoute,
     private patientService: PatientService,
@@ -48,19 +49,15 @@ export class AssignRoom implements OnInit {
   ) {}
 
   ngOnInit() {
-    this.roomId = Number(this.route.snapshot.paramMap.get('id'));
-
-    console.log(this.roomId);
+    this.roomId = this.data;
 
     if (this.roomId) {
-      console.log('ID de la habitación:', this.roomId);
+      console.log('Id de la habitación:', this.roomId);
 
-      this.roomService.getRoomById(this.roomId).subscribe((data) => {
-        this.room = data;
+      this.roomService.searchRooms(this.roomId).subscribe((data) => {
+        this.room = data[0];
       });
-      console.error('No completa el getRoomById');
 
-      // Cargar las camas usando el servicio, filtrando por idRoom, no posem disponibilitat perquè volem mostrar tots els llits
       this.bedService.getBedData(this.roomId).subscribe(
         (beds: BedInterface[]) => {
           this.beds = beds;
