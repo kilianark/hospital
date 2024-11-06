1ae457232f86b81c2e30377a8d84347e09ef72c3
Alex Gil <alexgpareja@icloud.com>
2024-11-06 10:18:31 +0100

    implementacion service hospital

--- angular-front/src/app/services/hospital.service.ts
+++ angular-front/src/app/services/hospital.service.ts
@@ -5,12 +5,45 @@ import { HospitalInterface } from '../interfaces/hospital.interface';
 
 @Injectable({
     providedIn: 'root'
-  })
-  
-  export class HospitalService {
-    private url = ""
-  
-    constructor (private http: HttpClient) { }
+})
 
-  }
-  
+export class HospitalService {
+    private apiUrl = "http://localhost:5124/api/Hospital"
+
+    constructor(private http: HttpClient) { }
+
+    // Método para obtener la lista de hospitales con filtros opcionales
+    getHospitals(hospitalCode?: number, hospitalName?: string): Observable<HospitalInterface[]> {
+        let params = new HttpParams();
+
+        if (hospitalCode) {
+            params = params.set('hospitalCode', hospitalCode.toString());
+        }
+        if (hospitalName) {
+            params = params.set('hospitalName', hospitalName);
+        }
+
+        return this.http.get<HospitalInterface[]>(this.apiUrl, { params });
+    }
+
+    // Método para obtener un hospital por ID
+    getHospitalById(id: number): Observable<HospitalInterface> {
+        return this.http.get<HospitalInterface>(`${this.apiUrl}/${id}`);
+    }
+
+    // Método para agregar un nuevo hospital
+    addHospital(hospital: HospitalInterface): Observable<HospitalInterface> {
+        return this.http.post<HospitalInterface>(this.apiUrl, hospital);
+    }
+
+    // Método para actualizar un hospital existente
+    updateHospital(id: number, hospital: HospitalInterface): Observable<void> {
+        return this.http.put<void>(`${this.apiUrl}/${id}`, hospital);
+    }
+
+    // Método para eliminar un hospital
+    deleteHospital(id: number): Observable<void> {
+        return this.http.delete<void>(`${this.apiUrl}/${id}`);
+    }
+
+}
