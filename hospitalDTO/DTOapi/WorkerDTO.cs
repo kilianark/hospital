0c40f2b9c1231aadd7fe85ba30b9f50eef7fba2f
Maria Manzano <mmanzanoroldan@gmail.com>
2024-11-14 09:40:31 +0100

    Hecho el DTO de worker

---
+++ hospitalDTO/DTOapi/WorkerDTO.cs
@@ -0,0 +1,20 @@
+namespace hospitalDTO.DTOapi {
+    public class WorkerDTO {
+    public required int id {get; set;}
+    public required string Name { get; set; }
+    public required string Surname1 { get; set; }
+    public string? Surname2 { get; set; }
+    public required string Dni { get; set; }
+    public required DateTime BirthDate { get; set; }
+    public required string Country { get; set; }
+    public string? Address {get; set;}
+    public required string Phone {get; set; }
+    public string? Email {get; set;}
+    public string? CIP {get; set;}
+    public required string Gender {get; set;}
+    public required string Username {get; set;}
+    public required string Worktype {get; set;}
+    public required string Speciality { get; set; }
+    public required int Hospital { get; set; }
+    }
+}
