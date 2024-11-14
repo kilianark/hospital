202d39a2c8f2215cb9b1162429d3bb8f0fbf33c5
Kilian <kilian08momo@gmail.com>
2024-11-14 12:12:31 +0100

    added role based control acces to rooms paths and workers path

--- angular-front/src/app/modules/room.module/room-routing.module.ts
+++ angular-front/src/app/modules/room.module/room-routing.module.ts
@@ -4,12 +4,13 @@ import { RoomComponent } from '../../pages/home/components/room/room.component';
 import { SearchRoomComponent } from '../../pages/home/components/room/search/search.component';
 import { CreateComponent } from '../../pages/home/components/room/create/create.component';
 import { ManageComponent } from '../../pages/home/components/room/manage/manage.component';
+import { RoleGuard } from '../../guard/role.guard';
 
 const routes: Routes = [
   { path: '', component: RoomComponent },
-  { path: 'search', component: SearchRoomComponent },
-  { path: 'create', component: CreateComponent },
-  { path: 'manage', component: ManageComponent},
+  { path: 'search', component: SearchRoomComponent, canActivate: [RoleGuard], data: { roles: ['search_room']} },
+  { path: 'create', component: CreateComponent, canActivate: [RoleGuard], data: { roles: ['create_room']} },
+  { path: 'manage', component: ManageComponent, canActivate: [RoleGuard], data: { roles: ['manage_room']}}, 
 ];
 
 @NgModule({
--- angular-front/src/app/modules/worker.module/worker-routing.module.ts
+++ angular-front/src/app/modules/worker.module/worker-routing.module.ts
@@ -3,9 +3,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreateWorkerComponent } from '../../pages/home/components/worker/create/create.component';
+import { RoleGuard } from '../../guard/role.guard';
 
 const routes: Routes = [
-  { path: 'create', component: CreateWorkerComponent },
+  { path: 'create', component: CreateWorkerComponent, canActivate: [RoleGuard], data: { roles: ['create_worker']} },
 ];
 
 @NgModule({
