aaacfa41bd7be8da8bd9c248a7569661165c51ea
Kilian <kilian08momo@gmail.com>
2024-11-15 10:42:04 +0100

    ahora deberia furular

--- angular-front/src/app/modules/home.module/home-routing.module.ts
+++ angular-front/src/app/modules/home.module/home-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { ProfileComponent } from '../../pages/home/components/profile/profile.component';
 import { HomeComponent } from '../../pages/home/components/home.component';
 import { AuthGuard } from '../../guard/auth.guard';
+import { RoleGuard } from '../../guard/role.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, canActivate: [AuthGuard] },
-  {path: 'patient',canActivate: [AuthGuard],loadChildren: () => import('../patient.module/patient.module').then(m => m.PatientModule)},
-  {path: 'room', canActivate: [AuthGuard], loadChildren: () => import('../room.module/room.module').then(m => m.RoomModule)},
-  {path: 'worker', canActivate: [AuthGuard], loadChildren: () => import('../worker.module/worker.module').then(m => m.WorkerModule)},
+  {path: 'patient',canActivate: [AuthGuard, RoleGuard], data: { roles: ['general_patients']}, loadChildren: () => import('../patient.module/patient.module').then(m => m.PatientModule)},
+  {path: 'room', canActivate: [AuthGuard, RoleGuard], data: { roles: ['general_rooms']}, loadChildren: () => import('../room.module/room.module').then(m => m.RoomModule)},
+  {path: 'worker', canActivate: [AuthGuard, RoleGuard], data: { roles: ['general_workers']}, loadChildren: () => import('../worker.module/worker.module').then(m => m.WorkerModule)},
   {path: 'profile', component: ProfileComponent, canActivate: [AuthGuard] }
 ];
 
