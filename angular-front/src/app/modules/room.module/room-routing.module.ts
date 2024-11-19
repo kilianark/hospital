ad9ee5a72c9910a211bd42806cf1babbf0e5f157
Kilian <kilian08momo@gmail.com>
2024-11-19 09:21:27 +0100

    authguardedall routes

--- angular-front/src/app/modules/room.module/room-routing.module.ts
+++ angular-front/src/app/modules/room.module/room-routing.module.ts
@@ -5,12 +5,13 @@ import { SearchRoomComponent } from '../../pages/home/components/room/search/sea
 import { CreateComponent } from '../../pages/home/components/room/create/create.component';
 import { ManageComponent } from '../../pages/home/components/room/manage/manage.component';
 import { RoleGuard } from '../../guard/role.guard';
+import { AuthGuard } from '../../guard/auth.guard';
 
 const routes: Routes = [
   { path: '', component: RoomComponent },
-  { path: 'search', component: SearchRoomComponent, canActivate: [RoleGuard], data: { roles: ['search_room']} },
-  { path: 'create', component: CreateComponent, canActivate: [RoleGuard], data: { roles: ['create_room']} },
-  { path: 'manage', component: ManageComponent, canActivate: [RoleGuard], data: { roles: ['manage_room']}}, 
+  { path: 'search', component: SearchRoomComponent, canActivate: [AuthGuard, RoleGuard], data: { roles: ['search_room']} },
+  { path: 'create', component: CreateComponent, canActivate: [AuthGuard, RoleGuard], data: { roles: ['create_room']} },
+  { path: 'manage', component: ManageComponent, canActivate: [AuthGuard, RoleGuard], data: { roles: ['manage_room']}}, 
 ];
 
 @NgModule({
