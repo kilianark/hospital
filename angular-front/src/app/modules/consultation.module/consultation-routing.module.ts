5e8e728e4958f81d701a3138500b219247e8e94b
KumiGaoo <Kumigaoo@gmail.com>
2024-11-20 12:55:07 +0100

    econsulta (no funciona pero al menos existe)

--- angular-front/src/app/modules/consultation.module/consultation-routing.module.ts
+++ angular-front/src/app/modules/consultation.module/consultation-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConsultationComponent } from '../../pages/home/components/consultation/consultation.component';
-
+import { EconsultaComponent } from '../../pages/home/components/consultation/econsulta/econsulta/econsulta.component';
+import { AuthGuard } from '../../guard/auth.guard';
+import { RoleGuard } from '../../guard/role.guard';
 const routes: Routes = [
   { path: '', component: ConsultationComponent },
+
+  { path: 'econsulta', component: EconsultaComponent, canActivate: [AuthGuard, RoleGuard], data: { roles: ['general_patients']} },
 ];
 
 @NgModule({
