35bad956636e76ff56cf48c67242b23213fec357
Maria Manzano <mmanzanoroldan@gmail.com>
2024-11-20 12:54:35 +0100

    Hecho el modulo de citas, y su ruta en  consultas

--- angular-front/src/app/modules/consultation.module/consultation-routing.module.ts
+++ angular-front/src/app/modules/consultation.module/consultation-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ConsultationComponent } from '../../pages/home/components/consultation/consultation.component';
+import { AppointmentComponent } from '../../pages/home/components/consultation/appointment/appointment.component';
 
 const routes: Routes = [
   { path: '', component: ConsultationComponent },
+  { path: 'appointment', component: AppointmentComponent}
 ];
 
 @NgModule({
