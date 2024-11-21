bea4946b8b2b71ec78da6d93dd85181786d2e3c0
Ashe7Amat <amatashenafi7@gmail.com>
2024-11-21 12:43:53 +0100

    ass

--- ApiHospital/Models/Person/Worker/Worker.cs
+++ ApiHospital/Models/Person/Worker/Worker.cs
@@ -2,7 +2,7 @@ namespace ApiHospital.Models
 {
     public abstract class Worker : Person
     {
-        public required int? WorkerCode {get; set;}
+        public required string WorkerCode {get; set;}
         public required string? Username { get; set; }
         public required string? Worktype { get; set; }
     }
