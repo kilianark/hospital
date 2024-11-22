e9112d1490c3120104e9816891901b069816472e
MelieCas <vcasares5@gmail.com>
2024-11-22 09:28:18 +0100

    Beds without soft delete

--- ApiHospital/Models/Bed.cs
+++ ApiHospital/Models/Bed.cs
@@ -1,9 +1,8 @@
 using System.ComponentModel.DataAnnotations;
-using ApiHospital.Interfaces;
 
 namespace ApiHospital.Models
 {
-    public class Bed : ISoftDelete
+    public class Bed 
     {
         [Key]
         public required int Id { get; set; }
