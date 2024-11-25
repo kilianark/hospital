53f16680c9e144f50b7bed3033c8a93df5139565
Kilian <kilian08momo@gmail.com>
2024-11-25 12:24:34 +0100

    created emergencies page

---
+++ angular-front/src/app/pages/home/components/emergencies/emergencies.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emergencies',
+  standalone: true,
+  imports: [],
+  templateUrl: './emergencies.component.html',
+  styleUrl: './emergencies.component.css'
+})
+export class EmergenciesComponent {
+
+}
