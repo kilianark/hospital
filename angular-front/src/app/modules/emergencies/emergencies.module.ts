a634d6b5bba7aec1bcf3222e252800e5c345632f
Kilian <kilian08momo@gmail.com>
2024-11-25 12:21:16 +0100

    added emergencies module

---
+++ angular-front/src/app/modules/emergencies/emergencies.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EmergenciesRoutingModule } from './emergencies-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    EmergenciesRoutingModule
+  ]
+})
+export class EmergenciesModule { }
