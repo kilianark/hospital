86aaa3cbc220caeb0ea89f4a16a9c1f2066f23cd
Kilian <kilian08momo@gmail.com>
2024-11-26 12:59:21 +0100

    lila haz magia

--- angular-front/src/app/pages/home/components/emergencies/pool-patients/pool-patients.component.ts
+++ angular-front/src/app/pages/home/components/emergencies/pool-patients/pool-patients.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PatientInterface } from '../../../../../interfaces/patient.interface';
+import { PatientService } from '../../../../../services/patient.service';
+import { HospitalInterface } from '../../../../../interfaces/hospital.interface';
 
 @Component({
   selector: 'app-pool-patients',
   templateUrl: './pool-patients.component.html',
   styleUrl: './pool-patients.component.css'
 })
-export class PoolPatientsComponent {
+export class PoolPatientsComponent implements OnInit {
+
+  patients: PatientInterface[] = [];
+  hospitals: HospitalInterface[] = [];
+
+  constructor(private patientService: PatientService) {}
+
+  ngOnInit(): void {
+      this.patientService.getPatientData().subscribe((data) => {
+        this.patients = data;
+      })
+  }
+
+  getHospitalName(hospitalCode: number): string {
+    const hospital = this.hospitals.find(h => h.hospitalCode === hospitalCode);
+    return hospital ? hospital.hospitalName : 'Desconocido';
+  }
 
 }
