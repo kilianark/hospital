cade3020b152f54205d95cf13aaad9de725e14dc
Kilian <kilian08momo@gmail.com>
2024-12-04 12:37:59 +0100

    refactring my-patients component

--- angular-front/src/app/pages/home/components/emergencies/my-patients/my-patients.component.ts
+++ angular-front/src/app/pages/home/components/emergencies/my-patients/my-patients.component.ts
@@ -6,7 +6,6 @@ import { HospitalService } from '../../../../../services/hospital.service';
 import { KeycloakService } from 'keycloak-angular';
 import { Router } from '@angular/router';
 import { DoctorService } from '../../../../../services/doctor.service';
-import { DoctorInterface } from '../../../../../interfaces/doctor.interface';
 import { AppointmentService } from '../../../../../services/appointment.service';
 
 @Component({
@@ -17,83 +16,43 @@ import { AppointmentService } from '../../../../../services/appointment.service'
 export class MyPatientsComponent implements OnInit{
 
   public patients: PatientInterface[] = [];
-  private hospitals: HospitalInterface[] = [];
-
-  private userRoles: string[];
-  public show: boolean = false;
-
-  private doctor: DoctorInterface;
   private doctorID: number;
 
   constructor(
-    private hospitalService: HospitalService,
     private appointmentService: AppointmentService,
     private keycloakService: KeycloakService,
-    private router: Router,
     private doctorService: DoctorService,
     private patientService: PatientService
-  ) {
-    
-    setTimeout(() => {
-      this.show = true;
-    }, 1);
-    
-    this.userRoles = this.keycloakService.getKeycloakInstance().realmAccess?.roles;
-    var hospitalNum: number = null;
-
-    if(this.userRoles.includes('Goldenfold')) hospitalNum = 1;
-    else if (this.userRoles. includes('HospitalFaro')) hospitalNum = 2;
-
-    this.loadPatientsData();
-    this.loadHospitalsData();
-  }
+  ) {}
 
   ngOnInit(): void {
-    
-  }
-
-  getHospitalName(hospitalCode: number): string {
-    const hospital = this.hospitals.find(h => h.hospitalCode === hospitalCode);
-    return hospital ? hospital.hospitalName : 'Desconocido';
-  }
-
-  loadHospitalsData(): void {
-    this.hospitalService.getHospitals().subscribe((hospitals) => {
-      this.hospitals = hospitals;
-    });
+    this.loadPatientsData();
   }
 
   loadPatientsData(): void {
-    //id doctor:
     var workerCode;
 
     this.keycloakService.loadUserProfile().then((profile) => {
       workerCode = profile.attributes['workerCode'][0];
       this.doctorService.getDoctorData(workerCode).subscribe((data) => {
-        if(data.length > 0) this.doctor = data[0];
-        this.doctorID = this.doctor.id;
+
+        this.doctorID = data[0].id;
 
         this.appointmentService.getAppointmentData(this.doctorID).subscribe((data) => {
 
           for(const appointment of data) {
-            var patientId = appointment.patientId;
 
-            this.patientService.getPatientById(patientId).subscribe((data) => { this.patients.push(data); });
+            this.patientService.getPatientById(appointment.patientId).subscribe((data) => { 
+              this.patients.push(data); 
+            });
+
           }
 
         });
+
       });
+
     });
 
-    
   }
-
-  assingPatient(patientId: number) {
-    //conseguir id del doctor
-
-
-    //enrutar al manage del patient
-    this.router.navigate(['/home/patient/manage', { id: patientId }]);
-  }
-
 }
