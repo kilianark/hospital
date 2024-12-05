06aa3d4d294cdeb390e5ee99718c37fa9734e102
Kilian <kilian08momo@gmail.com>
2024-12-05 10:08:01 +0100

    si

---
+++ angular-front/src/app/pages/home/components/patient/medical-episode/medical-episode.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-medical-episode',
+  standalone: true,
+  imports: [],
+  templateUrl: './medical-episode.component.html',
+  styleUrl: './medical-episode.component.css'
+})
+export class MedicalEpisodeComponent {
+
+}
