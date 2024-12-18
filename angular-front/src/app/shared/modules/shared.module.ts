6c5e63d338e780e087f185611186db49f7d0bc36
MelieCas <vcasares5@gmail.com>
2024-12-18 12:12:13 +0100

    Refactoring: Extract select doctor and add it to the create appointment (have to put the control for required)

--- angular-front/src/app/shared/modules/shared.module.ts
+++ angular-front/src/app/shared/modules/shared.module.ts
@@ -7,23 +7,26 @@ import { EnumToStringPipe } from "../../pipe/enum-to-string.pipe";
 import { ConfirmDialogComponent } from "../components/confirm-dialog/confirm-dialog.component";
 import SpinnerComponent from "../components/spinner/spinner.component";
 import { IdToStringPipe } from "../../pipe/id-to-string.pipe";
+import { DoctorSelectComponent } from "../components/doctor-select/doctor-select.component";
 
 @NgModule({
     declarations: [
         EnumToStringPipe,
         SpinnerComponent,
-        IdToStringPipe
+        IdToStringPipe,
     ],
     imports: [
         CommonModule,
         MaterialModule,
         ConfirmDialogComponent,
+        DoctorSelectComponent
     ],
     exports: [
         EnumToStringPipe,
         MaterialModule,
         ConfirmDialogComponent,
         SpinnerComponent,
+        DoctorSelectComponent,
         IdToStringPipe
     ]
 })
