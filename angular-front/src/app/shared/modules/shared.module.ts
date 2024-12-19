55e380349e672d43085fc608838bbe9659340fbd
MelieCas <vcasares5@gmail.com>
2024-12-19 13:20:07 +0100

    error solved

--- angular-front/src/app/shared/modules/shared.module.ts
+++ angular-front/src/app/shared/modules/shared.module.ts
@@ -8,10 +8,15 @@ import { ConfirmDialogComponent } from "../components/confirm-dialog/confirm-dia
 import SpinnerComponent from "../components/spinner/spinner.component";
 import { IdToStringPipe } from "../../pipe/id-to-string.pipe";
 import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
-import { HttpLoaderFactory } from "../../modules/home.module/home.module";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 import { HttpClient } from "@angular/common/http";
 import { DoctorSelectComponent } from "../components/selects/doctor-select/doctor-select.component";
 import { FormsModule } from "@angular/forms";
+
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
     declarations: [
