67eb19a53d862fcc25c7b8a1aa7841885ca15cc8
KumiGaoo <Kumigaoo@gmail.com>
2025-01-14 11:12:44 +0100

    wow que el css se importa

--- react-front/src/App.js
+++ react-front/src/App.js
@@ -1,26 +1,12 @@
-import logo from './assets/img/logo.svg';
 import './App.css';
 import Header from './app/components/navigate/header/header';
+import Home from './app/home/components/home';
 function App() {
   return (
     <div className="App">
         <Header />
         
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <a href="\app\components\navigate\header\header.jsx" className="fa fa-google">clickHere</a>
-      </header>
+        <Home />
     </div>
   );
 }
