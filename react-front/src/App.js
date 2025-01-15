1b5819a33524e1067e6150cf65830cb4d5cd66a7
KumiGaoo <Kumigaoo@gmail.com>
2025-01-15 09:58:21 +0100

    no funca

--- react-front/src/App.js
+++ react-front/src/App.js
@@ -1,16 +1,25 @@
-import './App.css';
-import Header from './app/components/navigate/header/header';
-import Home from './app/home/components/home';
+import { BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import "./App.css";
+import Header from "./app/components/navigate/header/header";
+import Home from "./app/home/components/home";
+import Calendar from "./app/home/components/patient/calendar/calendar";
 function App() {
   return (
-    <div className="App">
-      <div className="header">
-        <Header />
-      </div>
+    <Router>
+      <div className="App">
+        <div className="header">
+          <Header />
+        </div>
         <Home />
-    </div>
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/calendar" element={<Navigate />} />
+        <Route path="/appointment" element={<Calendar />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
-
--- react-front/src/app/home/components/home.jsx
+++ react-front/src/app/home/components/home.jsx
@@ -1,30 +1,44 @@
-import * as React from 'react';
-import './home.css';
-import CalendarImg from "../../../assets/lupa.png";   
+import * as React from "react";
+import "./home.css";
+import CalendarImg from "../../../assets/lupa.png";
 import CitasImg from "../../../assets/hab.png";
-function Home() {
-    
-  return (
-        <div id="mainBody">
-        <link rel="stylesheet" href="home.css" />
-        <div class="slogan">LA SALUD ORGANIZADA ES LA SALUD MEJORADA</div>
-        <div class="grid-layout">
-          <div appHasRole="general_patients">
-            <button class="calendar" onclick="window.location.href='/home/calendar';">
-              <img class="calendario" src={CalendarImg} alt="error de carga" />
-            </button>
-            <p>Calendario</p>
-          </div>
-          <div appHasRole="general_citas">
-            <button class="citas" onclick="window.location.href='/home/citas';">
-              <img class="citas" src={CitasImg} alt="error de carga" />
-            </button>
-            <p>Citas</p>
-          </div>
-        </div></div>
+import { useNavigate } from "react-router-dom";
+import Calendar from "./patient/calendar/calendar";
 
-    
+function Home() {
+  const navigate = useNavigate();
+
+  const goToCalendar = () => {
+    navigate("./calendar"); // Adjust path to match your routing setup
+  };
+
+  const goToCitas = () => {
+    navigate("./appointment"); // Adjust path to match your routing setup
+  };
+
+  return (
+    <div id="mainBody">
+      <link rel="stylesheet" href="home.css" />
+      <div className="slogan">LA SALUD ORGANIZADA ES LA SALUD MEJORADA</div>
+      <div className="grid-layout">
+        <div appHasRole="general_patients">
+          <button onClick={goToCalendar} className="calendar">
+            <img
+              className="calendario"
+              src={CalendarImg}
+              alt="error de carga"
+            />
+          </button>
+          <p>Calendario</p>
+        </div>
+        <div appHasRole="general_citas">
+          <button onClick={goToCitas} className="citas">
+            <img className="citas" src={CitasImg} alt="error de carga" />
+          </button>
+          <p>Citas</p>
+        </div>
+      </div>
+    </div>
   );
 }
-
 export default Home;
