b9880687802b116f336e2905a230e983b5c16833
KumiGaooITICBCN <2023_moises.diez@iticbcn.cat>
2025-01-17 12:51:24 +0100

    Calendario beta

--- react-front/src/app/home/components/patient/calendar/calendar.jsx
+++ react-front/src/app/home/components/patient/calendar/calendar.jsx
@@ -1,7 +1,39 @@
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import dayjs from "dayjs";
+import React, { useEffect } from "react";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+import { Button } from "@mui/material";
+
 function Calendar() {
+  // dayjs.tz.setDefault('Pacific/Auckland');
+  let date = new Date();
+  const [value, setValue] = React.useState(dayjs(date));
+
+  const handleClick = () => {
+    alert(`Your Appointment is booked on ${dayjs(value).format("DD-MM-YYYY")}`);
+  };
+
   return (
-    <div>
-      <h1>Calendar</h1>
+    <div className="Calendar">
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <DatePicker
+          value={value}
+          onChange={(newValue) => {
+            // console.log(newValue)
+            setValue(newValue);
+          }}
+        ></DatePicker>
+        <Button
+          variant="contained"
+          onClick={handleClick}
+          style={{ marginLeft: "70%" }}
+        >
+          BOOK APPOINTMENT
+        </Button>
+      </LocalizationProvider>
     </div>
   );
 }
